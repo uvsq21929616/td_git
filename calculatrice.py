8b2bc439120dda1711443e24a106710d05daa174
Elise Reber <elise.reber@ens.uvsq.fr>
2021-01-13 18:18:03 +0100

    création des boutons

---
+++ calculatrice.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+CANVAS_HEIGHT = 100
+CANVAS_WIDTH = 300
+
+root = tk.Tk()
+
+bouton_0 = tk.Button(root, text="0")
+bputon_1 = tk.Button(root, text="1")
+bouton_2 = tk.Button(root, text="2")
+bouton_3 = tk.Button(root, text="3")
+bouton_4 = tk.Button(root, text="4")
+bouton_5 = tk.Button(root, text="5")
+bouton_6 = tk.Button(root, text="6")
+bouton_7 = tk.Button(root, text="7")
+bouton_8 = tk.Button(root, text="8")
+bouton_9 = tk.Button(root, text="9")
+bouton_point = tk.Button(root, text=".")
+bouton_plus = tk.Button(root, text="+")
+tk.Button(root, text="-")
+tk.Button(root, text="x")
+tk.Button(root, text=":")
+
+
+root.mainloop()
